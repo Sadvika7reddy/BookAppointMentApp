e2ea953c8d8db19fa67c8abcd2307ecb1877f79e
Sadvika7reddy <sadvika7reddy@gmail.com>
2023-08-25 15:33:57 +0530

    added premium feature

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const bodyParser=require('body-parser');
 const express=require('express');
 const User=require('./models/users')
 const Expense=require('./models/expense')
+const Order=require('./models/orders')
 const sequelize=require('./util/database');
 
 const cors=require('cors')
 
 const applyRoutes=require('./routes/add-users')
 const expenseRoutes=require('./routes/add-expense');
+const purchaseRoutes=require('./routes/purchase')
 
 const app=express();
 
@@ -17,10 +19,13 @@ app.use(bodyParser.json({ extended: false }));
 
 app.use(applyRoutes);
 app.use(expenseRoutes)
+app.use(purchaseRoutes)
 
 User.hasMany(Expense)
 Expense.belongsTo(User)
 
+User.hasMany(Order)
+Order.belongsTo(User)
 
 sequelize.sync({})
 .then((result)=>{
---
+++ controllers/purchase.js
@@ -0,0 +1,54 @@
+const Razorpay=require('razorpay')
+const Order=require('../models/orders')
+
+const purchasepremium=async(req,res,next)=>{
+   try{
+        var rzp=new Razorpay({
+            //key_id:process.env.RAZORPAY_KEY_ID,
+              key_id:'rzp_test_UlCs5bCPfMifM7',
+            //key_secret:process.env.RAZORPAY_KEY_SECRET
+            key_secret:'qjlzOGlm1ClcNvlw6jg9CoOV'
+        })
+        const amount=2400;
+        rzp.orders.create({amount,currency:"INR"},(err,order)=>{
+            if(err){
+                throw new Error(JSON.stringify(err));
+            }
+             req.user.createOrder({orderid:order.id,status:'PENDING'}).then(()=>{
+                return res.status(201).json({order,key_id:rzp.key_id})
+             })
+            
+        })
+   }
+   catch(err){
+       return res.status(403).json({message:"something went wrong",error:err})
+   }
+}
+
+const updatetransactionstatus=async(req,res,next)=>{
+    try{
+       const {payment_id,order_id}=req.body;
+       let order=await Order.findOne({where:{orderid:order_id}}) 
+       if(order){
+
+        let promise1=order.update({paymentid:payment_id,status:'SUCCESSFUL'})
+        let promise2=req.user.update({ispremiumuser:true})
+        Promise.all([promise1,promise2]).then(()=>{
+            return res.status(202).json({sucess:true,message:'Transaction succesfulll'})
+        })
+         
+       }
+       else{
+        await order.update({paymentid:payment_id,status:'FAILED'})
+        return res.status(400).json({sucess:false,message:'Transaction Failed'})
+       }
+           } 
+    catch(err){
+       return res.status(403).json({sucess:false,message:'Transaction Failed'})
+    }
+}
+
+module.exports={
+    purchasepremium,
+    updatetransactionstatus
+}
